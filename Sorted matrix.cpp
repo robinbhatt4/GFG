0899df78109c86f1b1c8f2c5d17aa4826c6c04cf
robin bhatt <robinbhatt4@gmail.com>
2023-12-28 13:42:25 +0530

    First Commit

---
+++ Sorted matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+     
+       vector <int> ans;
+       for(int i=0;i<N;i++)
+       {
+           for(int j=0;j<N;j++)
+           {
+               ans.push_back(Mat[i][j]);
+           }
+       }
+       sort(ans.begin(),ans.end());
+       int k=0;
+       for(int i=0;i<N;i++)
+       {
+           for(int j=0;j<N;j++)
+           {
+               Mat[i][j]=ans[k++];
+           }
+       }
+      return Mat;  // code here
+// code here
+    }
+};
