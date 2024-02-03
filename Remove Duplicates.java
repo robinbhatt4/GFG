13ff34846c09d01f381c2237b894d2e30142031d
Robin Bhatt <robinbhatt4@gmail.com>
2024-02-03 12:07:18 +0530

    First commit

---
+++ Remove Duplicates.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeDups(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String removeDups(String S) {
+        // code here
+        Set<Character> ans=new HashSet<>();
+        String str="";
+        for(int i=0;i<S.length();i++)
+        {
+            char c=S.charAt(i);
+            if(ans.add(c))
+            {
+                str+=c;
+            }
+        }
+        return str;
+    }
+}
