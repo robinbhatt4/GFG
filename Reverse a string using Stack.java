2738aab82ada54ecd91800acab145da30512bf8c
Robin Bhatt <robinbhatt4@gmail.com>
2024-02-04 14:01:26 +0530

    First commit

---
+++ Reverse a string using Stack.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            Solution obj = new Solution();
+            System.out.println(obj.reverse(sc.next()));
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public String reverse(String S){
+        //code here
+        Stack<Character> s=new Stack<>();
+        String str="";
+        
+        for(int i=0;i<S.length();i++)
+        {
+            char c=S.charAt(i);
+            s.push(c);
+        }
+        while(!s.isEmpty())
+        {
+          str+=s.pop();  
+        }
+        return str;
+    }
+
+}
