fcc4d63c2a915b8e47381faa6fa9e837152594ef
Robin Bhatt <robinbhatt4@gmail.com>
2024-04-01 20:20:57 +0530

    First commit

---
+++ Sum of all prime numbers between 1 and N.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+    class GFG
+    {
+        public static void main(String[] args) throws IOException
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(br.readLine().trim());
+            while(T-->0)
+            {
+                int n = Integer.parseInt(br.readLine().trim());
+                Solution ob = new Solution();
+                long ans = ob.prime_Sum(n);
+                System.out.println(ans);
+            }
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    class Solution
+    {
+        public long prime_Sum(int n)
+        {long sum=0;
+            for(int i=2;i<=n;i++)
+        {
+            if(isPrime(i))
+            {sum+=i;}
+        }
+        return sum;
+            // code here
+        }
+        boolean isPrime(int k)
+        {
+            for(int i=2;i<=Math.sqrt(k);i++)
+            {
+                if(k%i==0)
+                return false;
+            }
+            return true;
+            
+        }
+    }
