7e6491dbf0301b9482e91bed96153341308bfe93
Robin Bhatt <robinbhatt4@gmail.com>
2024-04-20 14:03:13 +0530

    FIrst commit

---
+++ Panagram Checking.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for JAVA
+
+class Solution
+{
+    //Function to check if a string is Pangram or not.
+    public static boolean checkPangram  (String s) {
+        // your code here
+         HashSet<Character>set=new HashSet();
+        String p=s.toLowerCase();
+        for(int i=0;i<p.length();i++){
+            if(Character.isAlphabetic(s.charAt(i))){
+           set.add(p.charAt(i));}
+       }
+        if(set.size()!=26) return false;
+        return true;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	  int t=Integer.parseInt(br.readLine());
+	  while(t-->0)
+	  {
+	    String s=br.readLine().trim();
+	    
+	    System.out.println(new Solution().checkPangram (s)==true?1:0);
+	  }
+	  
+	 }
+}
+// } Driver Code Ends
