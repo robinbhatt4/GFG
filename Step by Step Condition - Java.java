dcee7fb816a2cbbff48b718c16a7a9247c4320b5
Robin Bhatt <robinbhatt4@gmail.com>
2024-04-24 21:21:11 +0530

    First commit

---
+++ Step by Step Condition - Java.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases count
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking n
+		    int n =sc.nextInt();
+		
+	        //creating an object of class Geeks	
+		    Geeks obj=new Geeks();
+		    
+		    //calling isDivisbleByPrime()
+		    //method of class Geeks
+		    obj.isDivisibleByPrime(n);
+		}
+	
+		
+	}
+}
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+//Back-end complete function Template for Java
+class Geeks {
+	 static void isDivisibleByPrime (int n) 
+	 {if(n%11==0){
+       System.out.print("Eleven");
+     }
+     else if(n%3==0){
+       System.out.print("Three");
+     }
+     
+       else if(n%2==0){
+       System.out.print("Two");
+     }
+   else{
+       System.out.print("-1");
+   }
+ System.out.println();      //Your code here
+       
+	 }
+
+}
+
+//{ Driver Code Starts.
+
+/*package whatever //do not write package name here */
+// } Driver Code Ends
